60d645b03d07949f1cb486c6ac775a6cb20fc99a
pieyx <ijujkh@gmail.com>
2024-04-21 09:48:25 +0300

    Initial Commit

---
+++ Keyn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Keyn
+//
+//  Created by Jood Khoja on 12/10/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Keyn/KeynApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeynApp.swift
+//  Keyn
+//
+//  Created by Jood Khoja on 12/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct KeynApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
